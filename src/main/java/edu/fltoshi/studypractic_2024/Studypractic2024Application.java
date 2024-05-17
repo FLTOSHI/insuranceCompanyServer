ac3ef033a3013eb8f9e5c9874b57c35dc40606c6
nik <nikgan@email.com>
2024-05-17 15:05:46 +0300

    Initial commit

---
+++ src/main/java/edu/fltoshi/studypractic_2024/Studypractic2024Application.java
@@ -0,0 +1,13 @@
+package edu.fltoshi.studypractic_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Studypractic2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Studypractic2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/fltoshi/studypractic_2024/Studypractic2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fltoshi.studypractic_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Studypractic2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
